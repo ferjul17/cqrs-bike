d1daf1259827f88b40ab3b4f35046b1a2079386a
Julien Ferrier <github@ferrier.email>
2021-01-01 23:02:54 +0100

    remove bicycle aggregate

--- src/cqrs/aggregate/bicycle/bicycle.aggregate.ts
+++
@@ -1,4 +0,0 @@
-import { AbstractAggregate } from '../abstract.aggregate';
-import { BicycleEvent } from './event/bicycle.event';
-
-export class BicycleAggregate extends AbstractAggregate<BicycleEvent> {}
--- src/cqrs/aggregate/bicycle/event/bicycle.event.ts
+++
@@ -1,3 +0,0 @@
-import { AbstractEvent } from '../../../abstract.event';
-
-export class BicycleEvent extends AbstractEvent {}
